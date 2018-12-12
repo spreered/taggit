69202886ee82e7662cfe65d9c86edb4f73505aaf
spreered <spreered@gmail.com>
2018-12-12 16:23:23 +0800

    add tag method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,19 @@
 class Post < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def self.tagged_with(name)
+    Tag.find_by!(name: name).posts
+  end
+
+  def tag_list
+    tags.map(&:name).join(', ')
+  end
+
+  def tag_list=(names)
+    self.tags = names.split(',').map do |item|
+      Tag.where(name: item.strip).first_or_create!
+    end
+  end
+
 end
